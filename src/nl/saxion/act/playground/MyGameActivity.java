cca9e5c836b40158628cab1d79c9c2e7a6d16108
George Harbers <george.harbers@gmail.com>
2013-03-01 11:21:04 +0100

    Timer links onderin het spel toegevoegd, moet alleen nog kijken hoe we de tijd kunnen gaan gebruiken.

--- src/nl/saxion/act/playground/MyGameActivity.java
+++ src/nl/saxion/act/playground/MyGameActivity.java
@@ -1,14 +1,19 @@
 package nl.saxion.act.playground;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.SystemClock;  // Deze wordt pas gebruikt op het moment dat het mogelijk is om de timer te resetten aan het eind van het level
+import android.widget.Chronometer;
 
 public class MyGameActivity extends Activity {
 	private GameView mGameView;
 	private final String TAG = "GameView";
 	private Button buttonFill, buttonCross, buttonBlank;
+	Chronometer mChronometer;
 	
 	//hoi
     /** Called when the activity is first created. */
@@ -23,6 +28,9 @@ public class MyGameActivity extends Activity {
         buttonFill = (Button) findViewById(R.id.buttonFill);
         buttonCross = (Button) findViewById(R.id.buttonCross);
         buttonBlank = (Button) findViewById(R.id.buttonBlank);
+        
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
+        mChronometer.start();
     }
     
     public void onButtonClicked(View v){
@@ -36,4 +44,16 @@ public class MyGameActivity extends Activity {
     		mGameView.setToAdd(GameView.HINT);
     	}
     }
+    
+//   Met mChronometer.stop(); wordt de timer stop gezet en dan kunnen we iets doen met die tijd. 
+//    Deze kan dus worden gecalled als het level over is.
+    
+//          
+//            mChronometer.stop();
+//       
+//  	
+//   Met de setBase wordt de timer weer op 00.00 gezet, deze kan worden gebruikt vlak voor het level begint.
+//            mChronometer.setBase(SystemClock.elapsedRealtime());
+//        
+//   
 }
