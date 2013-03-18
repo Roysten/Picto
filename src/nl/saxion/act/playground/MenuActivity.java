d29ba819d4c56168b01dcfe6bf254bfcac2db3ca
rickflits <rickflits@gmail.com>
2013-03-18 17:14:05 +0100

    Instructie toegevoegd

--- src/nl/saxion/act/playground/MenuActivity.java
+++ src/nl/saxion/act/playground/MenuActivity.java
@@ -7,19 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends Activity{
-	Button startButton, exitButton;
+	Button startButton, instructionButton;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
         
         startButton = (Button) findViewById(R.id.startbutton);
+        instructionButton = (Button) findViewById(R.id.instructionButton);
 	}
 	
 	public void onButtonClicked(View v) {
 		if(v == startButton){
 		  Intent intent = new Intent(this, PuzzlePickActivity.class);
 		  startActivity(intent);
+		}
+		else if (v == instructionButton){
+			Intent intent = new Intent(this, InstructionActivity.class);
+			startActivity(intent);
 		}
 	}
 }
